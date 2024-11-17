9f3ed8d836491f447ddc81fb0ad753005869fcbc
Divakar Jha <divakarjha9128@gmai.com>
2024-11-17 21:49:50 +0530

    first commit

---
+++ src/config/api.js
@@ -0,0 +1,7 @@
+const API_DOMAIN = "https://gnews.io/api/v4/top-headlines?country=";
+const API_SEARCH_DOMAIN = "https://gnews.io/api/v4/search?q=";
+const API_KEY = 'ba1bc4c13bec1ef16087b238553800c0';
+export const endpointPath = (country, category) =>
+  `${API_DOMAIN}${country}&lang=en&category=${category}&apikey=${API_KEY}`;
+export const endpointSearch = (searchQuery) =>
+  `${API_SEARCH_DOMAIN}${searchQuery}&lang=en&apikey=${API_KEY}`;
